a9a2eb86bbaaa417fcb1b77c5a59d35b3c329a17
Viral Solani <viral.solani@gmail.com>
2018-05-21 21:15:06 +0530

    State Management

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 class SearchBar extends Component {
 
-  render() {
-    //return <input onChange={this.onInputChage}/>;
-    return <input onChange={(event) => console.log(event.target.value)}/>;
+  constructor(props) {
+    super(props);
+
+    this.state = { term : ''};
   }
 
-  onInputChage(event) {
-    console.log(event.target.value);
+  render() {      
+    return (
+      <div>
+        <input onChange={(event) => this.setState({ term : event.target.value })}/>          
+      </div>
+    );
   }
-
 }
 
 export default SearchBar;
