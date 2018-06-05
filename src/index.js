743a15f930eaad4bf67b489ae9b6f5d72893b87c
Viral Solani <viral.solani@gmail.com>
2018-06-05 11:39:13 +0530

    passed props to video components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from "react-dom";
 import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
 const API_KEY = "AIzaSyBFwF8wZP87EcdMoNb0Vy8a4nHRy1CmV5Y";
 
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
