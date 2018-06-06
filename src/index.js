d7b601f4511cd8659d8f7cb6b3c8cb86ac15641e
Viral Solani <viral.solani@gmail.com>
2018-06-06 10:54:44 +0530

    Search videos

--- src/index.js
+++ src/index.js
@@ -16,8 +16,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch("surf-boards");
+  }
 
-    YTSearch({ key: API_KEY, term: "surf-boards" }, videos => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
