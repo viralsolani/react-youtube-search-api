e6a1af26f071330873634553d4ea88fab32572b8
Viral Solani <viral.solani@gmail.com>
2018-06-06 11:06:11 +0530

    add placholder in search input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,13 +4,14 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: "Starting Value" };
+    this.state = { term: "" };
   }
 
   render() {
     return (
       <div className="search-bar">
         <input
+          placeholder="Search Here"
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)}
         />
