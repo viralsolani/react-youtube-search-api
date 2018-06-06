49cd275ad00d176ed55c454521e549284d8ffaad
Viral Solani <viral.solani@gmail.com>
2018-06-06 10:28:35 +0530

    - Handled Null Props - Video Select Feature by callback function

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const videoTitle = video.snippet.title;
 
-    const imageUrl      =  video.snippet.thumbnails.default.url;
-    const videoTitle    =  video.snippet.title; 
- 
-    return ( 
-        <li className="list-group-item">
-            <div className="video-list media">
-                <div className="media-left">
-                    <img className="media-object" src= { imageUrl } /> 
-                </div>
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
 
-                <div className="media-body">
-                    <div className="media-heading"> { videoTitle } </div>
-                </div>
-            </div>
-        </li>
-    );
-  };
-  
-  export default VideoListItem;
-  
+        <div className="media-body">
+          <div className="media-heading"> {videoTitle} </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyBFwF8wZP87EcdMoNb0Vy8a4nHRy1CmV5Y";
 
@@ -11,11 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: "surf-boards" }, videos => {
-      //this.setState({ videos: videos });
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -23,14 +29,14 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
 }
-
-/*const App = () => {
-  return <SearchBar />;
-};*/
 
 ReactDom.render(<App />, document.querySelector(".container"));
